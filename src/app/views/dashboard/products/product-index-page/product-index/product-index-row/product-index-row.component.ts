f98dbd30b79d6c4f0b1d3fc57741390306552501
angel valverde <angelvalverde21@gmail.com>
2025-08-06 14:23:27 -0500

    x

---
+++ src/app/views/dashboard/products/product-index-page/product-index/product-index-row/product-index-row.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-product-index-row',
+  imports: [],
+  templateUrl: './product-index-row.component.html',
+  styleUrl: './product-index-row.component.scss'
+})
+export class ProductIndexRowComponent {
+
+}
--- src/app/views/dashboard/shared/templates/product-list-template/product-list-template.component.ts
+++ src/app/views/dashboard/shared/templates/product-list-template/product-list-template.component.ts
@@ -2,14 +2,19 @@ import { Component, Input } from '@angular/core';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { CommonModule, JsonPipe } from '@angular/common';
 import { ButtonLinkComponent } from '../../components/buttons/button-link/button-link.component';
-import { faEdit } from '@fortawesome/free-solid-svg-icons';
-import { environment } from 'src/app/core/environments/environment';
+import { faEdit, faTrash } from '@fortawesome/free-solid-svg-icons';
+
+import { ButtonComponent } from '../../components/buttons/button/button.component';
+import { environment } from '../../../../../core/environments/environment';
+import Swal from 'sweetalert2';
+
 
 @Component({
   selector: 'app-product-list-template',
   imports: [
     FontAwesomeModule,
     CommonModule,
+    ButtonComponent,
     ButtonLinkComponent
   ],
   templateUrl: './product-list-template.component.html',
@@ -17,11 +22,39 @@ import { environment } from 'src/app/core/environments/environment';
 })
 export class ProductListTemplateComponent {
   faEdit = faEdit;
+  faTrash = faTrash;
 
   @Input() products: any[] = [];
 
   onImageError(event: Event) {
     (event.target as HTMLImageElement).src =
       environment.imageThumbnailPlaceHolderVertical;
+  }
+
+  archivar(product_id: number){
+
+    Swal.fire({
+      title: '¿Estás seguro?',
+      text: "No podrás deshacer esta acción",
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Sí, eliminar',
+      cancelButtonText: 'No, cancelar'
+    }).then((result) => {
+      if (result.isConfirmed) {
+        // Acción para eliminar el elemento
+
+        
+
+        Swal.fire(
+          'Eliminado!',
+          'El elemento ha sido eliminado.',
+          'success'
+        );
+      }
+    });
+    
   }
 }
