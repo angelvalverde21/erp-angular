49ca4ea01689f42ab400dc2b620b5dbbe382ba79
Angel Laptop dell <angelvalverde21@gmail.com>
2026-04-08 23:44:59 -0500

    x

--- src/app/views/dashboard/inventories/variants/inventory-variant-index/inventory-variant-index.component.ts
+++ src/app/views/dashboard/inventories/variants/inventory-variant-index/inventory-variant-index.component.ts
@@ -1,138 +1,65 @@
-﻿import { Component, EventEmitter, Input, OnInit, Output, SimpleChanges } from '@angular/core';
-import { ButtonComponent } from '../../../../shared/components/buttons/button/button.component';
-import { faBarcode, faCheck } from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+﻿import { JsonPipe } from '@angular/common';
+import { Component, Input, OnInit, SimpleChanges } from '@angular/core';
 import { FormArray, FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
-import { JsonPipe } from '@angular/common';
-
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faBarcode, faCheck } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-inventory-variant-index',
+  standalone: true,
   imports: [
-    ButtonComponent,
-    FontAwesomeModule,
     ReactiveFormsModule,
+    FontAwesomeModule,
     JsonPipe
   ],
-  templateUrl: './inventory-variant-index.component.html',
-  styleUrl: './inventory-variant-index.component.scss'
+  templateUrl: './inventory-variant-index.component.html'
 })
 export class InventoryVariantIndexComponent implements OnInit {
 
-
   @Input() variants: any[] = [];
-  @Output() inventoryVariantAdded = new EventEmitter<any>();
-
-  @Output() inventoryVariantArraySelected = new EventEmitter<any>();
-  @Output() emitInventoryVariantsSelected = new EventEmitter<any>();
-
-  inventory_variant_select: any;
 
   faBarcode = faBarcode;
   faCheck = faCheck;
 
-  formChildrenIsValid: boolean = false;
-
-  constructor(
-    private fb: FormBuilder,
-  ) { }
-
   form!: FormGroup;
 
-  formInit() {
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.buildForm();
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes['variants'] && this.variants?.length) {
+      this.buildForm();
+    }
+  }
+
+  private buildForm() {
     this.form = this.fb.group({
-      variantsForm: this.fb.array([])
+      variantsForm: this.fb.array(
+        this.variants.map(v =>
+          this.fb.group({
+            id: [v.id],              // 👈 guardamos id dentro del form
+            quantity: ['']
+          })
+        )
+      )
+    });
+
+    // 🔥 Debug reactivo real
+    this.form.valueChanges.subscribe(value => {
+      console.log('FORM VALUE:', value);
     });
   }
 
   get variantsForm(): FormArray {
-    return this.form.controls['variants'] as FormArray;
+    return this.form.get('variantsForm') as FormArray;
   }
 
-  ngOnInit(): void {
-
-    this.formInit();
-
-    this.variants.forEach(variant => {
-      this.addQuantity();
-    });
-
-    // this.inventoryVariantSelectedInit();
-  }
-
-  addQuantity() {
-
-    const group = this.fb.group({
-      sku: [''],
-      stock: [0]
-    });
-
-    this.variantsForm.push(group);
-  }
-
-  inventoryVariantSelectedInit() {
-
-    this.inventory_variant_select = {};
-
-    this.variants.forEach(variant => {
-      this.inventory_variant_select = {
-        ...this.inventory_variant_select,
-        [variant.id]: false
-      };
-    })
-
-    console.log(this.inventory_variant_select);
-  }
-
-  addVariant(variantId: number) {
-
-    if (this.inventory_variant_select[variantId]) {
-      this.inventory_variant_select[variantId] = false;
-    } else {
-      this.inventory_variant_select[variantId] = true;
-    }
-
-    console.log(this.inventory_variant_select);
-
-    // {333: true, 334: false, 335: true, 378: false, 395: true, 2652: false, 2653: false}
-
-    //Esto convierte el objeto en un array con los ids seleccionados (el objeto de arriba quedarÃ­a [333, 335, 395])
-    const inventoryVariantsSelected = Object.entries(this.inventory_variant_select)
-      .filter(([_, value]) => value === true)
-      .map(([key]) => Number(key));
-
-    // y aqui se exporta o emite el array
-    this.inventoryVariantArraySelected.emit(inventoryVariantsSelected);
-
-    // this.inventoryVariantArraySelected.emit(inventoryVariantsSelected);
-    // const variant = this.variants.find(v => v.id === variantId);
-
-    // if (variant) {
-    //   this.inventoryVariantAdded.emit(this.inventory_variant_select);
-    // }
-
-    const obj = this.variants.filter(v => inventoryVariantsSelected.includes(v.id));
-
-    console.log(obj);
-
-
-    this.emitInventoryVariantsSelected.emit(obj);
-
-
-  }
-
-  ngOnChanges({ variants }: SimpleChanges) {
-
-    console.log("variants changed", variants);
-
-    if (variants?.currentValue?.length) {
-      this.inventoryVariantSelectedInit();
-
-    }
-  }
-
+  // addVariant(index: number) {
+  //   const group = this.variantsForm.at(index);
+  //   console.log('Variant selected:', group.value);
+  // }
 
 }
-
-
-
