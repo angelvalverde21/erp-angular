b7eeece1527807bfa070492d67e1aa36fbf2b268
Angel Valverde <angelvalverde21@gmail.com>
2026-05-17 08:32:26 -0500

    x

--- src/app/views/dashboard/users/employees/employe-index-row/employe-index-row.component.ts
+++ src/app/views/dashboard/users/employees/employe-index-row/employe-index-row.component.ts
@@ -8,7 +8,7 @@ import { BaseService } from 'src/app/views/base.service';
 import { ButtonEditComponent } from 'src/app/views/shared/components/buttons/button-edit/button-edit.component';
 import { DateShopifyPipe } from 'src/app/views/shared/pipes/date-shopify.pipe';
 import { StoreService } from 'src/app/views/stores/store.service';
-
+import { PenPipe } from '@shared/pipes/pen.pipe'
 @Component({
   selector: 'tr[app-employe-index-row]',
   imports: [
@@ -17,12 +17,13 @@ import { StoreService } from 'src/app/views/stores/store.service';
     NgbDropdownModule,
     ButtonEditComponent,
     FontAwesomeModule,
-    RouterModule
+    RouterModule,
+    PenPipe
   ],
   templateUrl: './employe-index-row.component.html',
   styleUrl: './employe-index-row.component.scss'
 })
-export class EmployeIndexRowComponent implements OnInit{
+export class EmployeIndexRowComponent implements OnInit {
 
   faIdBadge = faIdBadge;
   faSackDollar = faSackDollar;
@@ -30,18 +31,24 @@ export class EmployeIndexRowComponent implements OnInit{
   faCoins = faCoins;
   faClock = faClock;
 
+  minuts_base = 14400;
+  factor: number = 0;
+
   @Input() employee: any = {};
 
   store: string = '';
 
   constructor(
     private _base: BaseService
-  ){
+  ) {
 
     // console.log(this.store);
-    
+
   }
   ngOnInit(): void {
-    this.store = this._base.storeName || '';  }
+    this.store = this._base.storeName || '';
+    this.factor = this.employee.salary / this.minuts_base;
+
+  }
 
 }
